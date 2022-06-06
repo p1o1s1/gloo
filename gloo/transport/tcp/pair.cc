85bda0e158103e03d2d35e852d2f9a9fd7a8e0ed
p1o1s1 <731650980@qq.com>
2022-06-06 11:00:02 +0800

    Update pair.cc

--- gloo/transport/tcp/pair.cc
+++ gloo/transport/tcp/pair.cc
@@ -156,7 +156,7 @@ void Pair::connect(const Address& peer) {
 
   // Addresses have to have same family
   if (selfAddr.ss_family != peerAddr.ss_family) {
-    printf("selfAddr.ss_family=%s, peerAddr.ss_family=%s", selfAddr.ss_family, peerAddr.ss_family);
+    cout << "selfAddr.ss_family=" << selfAddr.ss_family << "peerAddr.ss_family=" << peerAddr.ss_family << endl;
     GLOO_THROW_INVALID_OPERATION_EXCEPTION("address family mismatch");
   }
 
