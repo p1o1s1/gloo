3863799dec3d3bd09d6f74b14884f8db790acacf
p1o1s1 <731650980@qq.com>
2022-06-12 11:26:34 +0800

    Update buffer.cc

--- gloo/transport/tcp/buffer.cc
+++ gloo/transport/tcp/buffer.cc
@@ -127,7 +127,7 @@ void Buffer::send(size_t offset, size_t length, size_t roffset) {
   // to support this.
   GLOO_ENFORCE_LE(offset + length, size_);
 
-  if (debug_) {
+  if (1) {
     std::cout << "[" << getpid() << ": " << syscall(__NR_gettid) << "] ";
     std::cout << "send " << length << " bytes";
     std::cout << " to " << pair_->peer().str();
