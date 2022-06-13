1c8f8745c1a287d6f559ac2832f31e8a18048504
p1o1s1 <731650980@qq.com>
2022-06-13 16:34:30 +0800

    Update context.cc

--- gloo/rendezvous/context.cc
+++ gloo/rendezvous/context.cc
@@ -222,7 +222,11 @@ std::shared_ptr<::gloo::Context> ContextFactory::makeContext(
     recvBuffers_[i]->waitRecv();
     std::cout << "111" <<std::endl;
     auto& data = recvData_[i];
-    std::cout <<"data = " <<data <<std::endl;
+    std::cout <<"data =";
+    for(auto d: data) {
+        std::cout << d ;
+    }
+     std::cout << std::endl;
     auto address = std::vector<char>(data.begin() + PREAMBLE_LEN, data.begin() + PREAMBLE_LEN + addressSize);
     transportContext->getPair(i)->connect(address);
 
