259afefc46ae388bef82e48d3ad088f75ae320c1
p1o1s1 <731650980@qq.com>
2022-06-19 11:41:08 +0800

    Update context.cc

--- gloo/rendezvous/context.cc
+++ gloo/rendezvous/context.cc
@@ -231,9 +231,9 @@ std::shared_ptr<::gloo::Context> ContextFactory::makeContext(
 
   context->device_ = dev;
   context->transportContext_ = std::move(transportContext);
-  return std::static_pointer_cast<::gloo::Context>(context);
 
   std::cout << "you have been out 3" <<std::endl;
+  return std::static_pointer_cast<::gloo::Context>(context);
 }
 
 } // namespace rendezvous
