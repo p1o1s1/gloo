6e080955c9a0416aae7ca929c8101b728f228fb0
p1o1s1 <731650980@qq.com>
2022-06-19 15:29:47 +0800

    Update pair.cc

--- gloo/transport/tcp/pair.cc
+++ gloo/transport/tcp/pair.cc
@@ -554,7 +554,7 @@ bool Pair::read() {
     // Note that this means that the buffer pointer has been
     // set, per the call to prepareRead.
     std:: cout << "prepareRead nbytes =" << nbytes << std::endl;
-    if (nbytes == sizeof(op.preamble)) {
+    if (nbytes == sizeof(rx_.preamble)) {
       break;
     }
 
